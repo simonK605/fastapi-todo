d0f839f9d632d4365fde3963b4cd7b59161c34d5
Simon Karapetyan <simon.karapetyan@esterox.am>
2025-05-29 16:29:23 +0400

    Created api to store todo

--- app/schemas/todo.py
+++ app/schemas/todo.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel
 class ToDoBase(BaseModel):
     title: str
 
+class ToDoCreate(ToDoBase):
+    checked: bool = False
+
 class ToDoOut(ToDoBase):
     id: int
     checked: bool
